d93538801790a7cc1a490c7e9c264979b0fa07d0
NacherBeats <ibentivoglioacademico@gmail.com>
2026-04-17 16:30:13 -0300

    Se crea el componente para error 404, página no encontrado

---
+++ 1ADevelopers/frontend/src/app/pages/notfound404/notfound404.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound404',
+  imports: [],
+  templateUrl: './notfound404.html',
+  styleUrl: './notfound404.css',
+})
+export class Notfound404 {}
