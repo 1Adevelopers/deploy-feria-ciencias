c10ea8d0b55145fe0b2c9eb6b882a0a3a76b33fc
Florencia Lorenzati <florencialorenzati@gmail.com>
2026-04-18 16:43:11 -0300

    se crea componente quienes somos

--- 1ADevelopers/frontend/src/app/pages/home/home.ts
+++ 1ADevelopers/frontend/src/app/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { Contact } from '../contact/contact';
-
+import { QuienesSomos } from '../quienes-somos/quienes-somos';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Contact],
+  imports: [Header, Contact, QuienesSomos],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
