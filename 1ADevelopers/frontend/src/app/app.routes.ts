077b0f83776b35f822953059f4baae82894d356e
Dario <121806374+dariodev18@users.noreply.github.com>
2026-05-06 20:22:29 -0300

    Se crea formulario alta de usuario en dashboard

--- 1ADevelopers/frontend/src/app/app.routes.ts
+++ 1ADevelopers/frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 import { Contact } from './pages/contact/contact';
 import { Notfound404 } from './pages/notfound404/notfound404';
 import { Enproceso } from './shared/enproceso/enproceso';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'dashboard', component: Dashboard },
   { path: 'quienes-somos', component: QuienesSomos },
   { path: 'galeria', component:  Enproceso},
   { path: 'contacto', component: Contact },
