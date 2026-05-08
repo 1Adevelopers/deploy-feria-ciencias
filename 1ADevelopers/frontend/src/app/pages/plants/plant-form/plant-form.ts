cd28bcd04ec204387231408eb22f8e7ceb0063d5
Dario <121806374+dariodev18@users.noreply.github.com>
2026-05-07 23:33:57 -0300

    Se modifican archivos html dashboard y sidebar y  se enrutan los componentes

--- 1ADevelopers/frontend/src/app/pages/plants/plant-form/plant-form.ts
+++ 1ADevelopers/frontend/src/app/pages/plants/plant-form/plant-form.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-plant-form',
+  standalone:true,
   imports: [],
   templateUrl: './plant-form.html',
   styleUrl: './plant-form.css',
