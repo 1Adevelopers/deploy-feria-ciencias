f85297803e5be94097dbb02e0b74e1d22e69b51a
Florencia Lorenzati <florencialorenzati@gmail.com>
2026-05-12 16:37:17 -0300

    feat: formulario reactivo de contacto

--- 1ADevelopers/frontend/src/app/pages/contact/contact.ts
+++ 1ADevelopers/frontend/src/app/pages/contact/contact.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrls: ['./contact.css'],
 })
-export class Contact {}
+export class Contact {
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  enviarConsulta() {
+    const email = this.form.value.email;
+    const mensaje = this.form.value.mensaje;
+
+    if (this.form.valid) {
+      alert('¡Tu consulta fue enviada con éxito!');
+      console.log('Consulta enviada:', { email, mensaje });
+
+      this.router.navigate(['home']);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
