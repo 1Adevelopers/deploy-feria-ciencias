1bb10b8eddae6ffc21056ec98f390ff31b55c182
NacherBeats <ibentivoglioacademico@gmail.com>
2026-06-05 18:08:31 -0300

    Feat: Se agrega botón que envia a formulario de registro y se arregla titulo en formularios

--- 1ADevelopers/frontend/src/app/pages/auth/login/login.ts
+++ 1ADevelopers/frontend/src/app/pages/auth/login/login.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
