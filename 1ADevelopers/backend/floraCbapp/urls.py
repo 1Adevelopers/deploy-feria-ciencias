c25c9ae4e7ba15c55d0b61c50f05b29aa3842f91
Florencia Lorenzati <florencialorenzati@gmail.com>
2026-06-09 09:28:36 -0300

    refactor: se elimina aplicación obsoleta fichas

--- 1ADevelopers/backend/floraCbapp/urls.py
+++ 1ADevelopers/backend/floraCbapp/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('api/usuarios/', include('usuarios.urls')),
     path('api/flora/', include('flora.urls')),
     path('api/interacciones/', include('interacciones.urls')),
-    path('api/fichas/', include('fichas.urls')),
 ]
