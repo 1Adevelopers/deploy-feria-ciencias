3e82bfe686c3b7da804ef25ce41a9224e5ecd139
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2026-06-24 16:21:47 -0300

    Configuración de base de datos en la nube y settings para deploy

--- 1ADevelopers/backend/floraCbapp/settings.py
+++ 1ADevelopers/backend/floraCbapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','*']
 
 
 # Application definition
@@ -131,7 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Agrega todos los Archivos Estáticos a una sola carpeta (CSS, JavaScript, Backend Admin)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Archivos Media (Imágenes que suben los usuarios a una sola carpeta)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 CORS_ALLOWED_ORIGINS = [
